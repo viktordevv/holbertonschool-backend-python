169cbb9fb980106228510215f146c8734ea1c5f1
Viktor <viktorkrilldj@gmail.com>
2022-12-09 15:35:29 -0500

    upload files

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+""" Takes list input of of integers and floats, returns float """
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ Sums list of of integers and floats """
+    return sum(mxd_lst)
---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+""" Annotates the below function’s parameters """
+
+from typing import List, Tuple, Sequence, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ Duck typing """
+    return [(i, len(i)) for i in lst]
